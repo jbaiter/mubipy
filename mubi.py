2d27371cf8f1d72014cb7335818c5ed316b2299e
Johannes Baiter <johannes.baiter@gmail.com>
2012-09-06 00:04:51 +0200

    _get_shortdetails returns (Film, VideoMetadata) tuple; Cosmetic adjustments

--- mubi.py
+++ mubi.py
@@ -2,16 +2,16 @@
 
 # Copyright (c) 2012, Johannes Baiter (jbaiter)
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met: 
-# 
+# modification, are permitted provided that the following conditions are met:
+#
 # 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer. 
+#    this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution. 
-# 
+#    and/or other materials provided with the distribution.
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -41,18 +41,19 @@ VideoMetadata = namedtuple('VideoMetadata',
                            ['year', 'rating', 'cast', 'director', 'plot',
                             'title', 'originaltitle', 'duration', 'writer',
                             'playcount', 'trailer', 'audio_language',
-                            'subtitle_language'])
+                            'subtitle_language', 'plotoutline'])
+
 
 class Mubi(object):
     _URL_MUBI = "http://mubi.com"
     _URL_MUBI_SECURE = "https://mubi.com"
-    _regexps = { "item":        re.compile("item.*"),
-                 "watch_link":  re.compile("watch_link.*"),
-                 "duration":    re.compile("\d+ Min"),
-                 "program":     re.compile("use6.*"),
-                 "rating":      re.compile(r"Currently ([1-5]\.\d)/5 Stars."),
-                 "audio_lang":  re.compile(r"Audio in (.*)"),
-                 "sub_lang":    re.compile(r"Subtitled in (.*)")}
+    _regexps = {"item":        re.compile("item.*"),
+                "watch_link":  re.compile("watch_link.*"),
+                "duration":    re.compile("\d+ Min"),
+                "program":     re.compile("use6.*"),
+                "rating":      re.compile(r"Currently ([1-5]\.\d)/5 Stars."),
+                "audio_lang":  re.compile(r"Audio in (.*)"),
+                "sub_lang":    re.compile(r"Subtitled in (.*)")}
     _mubi_urls = {
                   "login":      urljoin(_URL_MUBI_SECURE, "login"),
                   "session":    urljoin(_URL_MUBI_SECURE, "session"),
@@ -83,14 +84,16 @@ class Mubi(object):
         self.genres = self._parse_genres(list_page)
         self.languages = self._parse_languages(list_page)
         self.countries = self._parse_countries(list_page)
-        
+
     def __del__(self):
         self._session.get(self._mubi_urls["logout"])
 
     def _parse_watchable_titles(self, page):
-        items = [x for x in BS(page).findAll("div", {"class": self._regexps["item"]})
+        items = [x for x in BS(page).findAll("div",
+                                             {"class": self._regexps["item"]})
                  if (x.findChild("h2")
-                 and x.findChild("div", {"class": self._regexps["watch_link"]}))]
+                 and x.findChild("div",
+                                 {"class": self._regexps["watch_link"]}))]
         return [Film(title=x.find("h2").text,
                      mubi_id=x.get("data-item-id"),
                      filmstill=(x.find("div", "cropped_image")
@@ -103,9 +106,22 @@ class Mubi(object):
                           .content)
 
     def _get_shortdetails(self, mubi_id):
-        return json.loads(self._session.get(self._mubi_urls["shortdetails"]
+        # Available keys: cast, directors, duration [minutes], excerpt,
+        #                 id, primary_country, title, year
+        info = json.loads(self._session.get(self._mubi_urls["shortdetails"]
                                             % unicode(mubi_id)
-            ).content)
+                         ).content)
+        return (Film(info['title'], info['id'],
+                     self._get_filmstill(self._resolve_id(info['id']))),
+                VideoMetadata(year=info['year'], rating=None,
+                              cast=info['cast'].split(", "),
+                              director=", ".join(info['director'].values()),
+                              plot=None, title=info['title'],
+                              originaltitle=None, duration=info['duration'],
+                              writer=None, playcount=None, trailer=None,
+                              audio_language=None, subtitle_language=None,
+                              plotoutline=info['excerpt'])
+               )
 
     def _get_filmstill(self, name):
         return self._mubi_urls["filmstill"] % (name, name)
@@ -147,9 +163,9 @@ class Mubi(object):
         rating = float(self._regexps["rating"].match(
                  page.find("li", "current_rating").text)
                  .group(1))
-        cast = [x.text.replace("CAST","").split(",")
+        cast = [x.text.replace("CAST", "").split(",")
                 for x in full_cast if x.span.text == 'CAST'][0]
-        director = [x.text.replace("DIR","").replace(",",", ")
+        director = [x.text.replace("DIR", "").replace(",", ", ")
                     for x in full_cast if x.span.text == 'DIR'][0]
         plot = "\n".join([x.text for x in
                           page.find("div", "content greenbg clear")
@@ -182,19 +198,22 @@ class Mubi(object):
                              originaltitle=originaltitle, duration=duration,
                              writer=writer, playcount=playcount,
                              trailer=trailer, audio_language=audio_language,
-                             subtitle_language=subtitle_language)
+                             subtitle_language=subtitle_language,
+                             plotoutline=None)
 
     def login(self, username, password):
         self._username = username
         self._password = password
         login_page = self._session.get(self._mubi_urls["login"]).content
-        auth_token = BS(login_page).find("input", {"name": "authenticity_token"}).get("value")
-        session_payload = { 'utf8': '✓',
-                            'authenticity_token': auth_token,
-                            'email': username,
-                            'password': password,
-                            'x': 0,
-                            'y': 0 }
+        auth_token = (BS(login_page).find("input",
+                                          {"name": "authenticity_token"})
+                      .get("value"))
+        session_payload = {'utf8': '✓',
+                           'authenticity_token': auth_token,
+                           'email': username,
+                           'password': password,
+                           'x': 0,
+                           'y': 0}
         landing_page = self._session.post(self._mubi_urls["session"],
                                           data=session_payload)
         self._userid = BS(landing_page.content).find(
@@ -202,9 +221,10 @@ class Mubi(object):
 
     def is_film_available(self, name):
         if not self._session.head(self._mubi_urls["video"] % name):
-            prescreen_page = self._session.get(self._mubi_urls["prescreen"] % name)
+            prescreen_page = self._session.get(self._mubi_urls["prescreen"]
+                                               % name)
             if not prescreen_page:
-                raise Exception("Ooops, something went wrong while scraping :-(")
+                raise Exception("Oops, something went wrong while scraping :(")
             else:
                 availability = BS(prescreen_page.content).find(
                         "div", "film_viewable_status ").text
@@ -217,7 +237,6 @@ class Mubi(object):
             raise Exception("This film is not available in your country.")
         return self._session.get(self._mubi_urls["video"] % name).content
 
-    
     def search_film(self, term):
         results = self._search(term)
         filtered = [x for x in results if x['category'] == "Films"]
@@ -234,16 +253,18 @@ class Mubi(object):
                         portrait=self._get_person_image(x['id']))
                  for x in results if x['category'] == "People"]
         return final
-    
+
     def get_person_films(self, person_id):
         person_page = self._session.get(self._mubi_urls["person"] % person_id)
         return self._parse_watchable_titles(person_page.content)
 
-    def get_all_films(self, page=1, sort_key='popularity', genre=None, country=None, language=None):
+    def get_all_films(self, page=1, sort_key='popularity', genre=None,
+                      country=None, language=None):
         if sort_key not in self._SORT_KEYS:
-            raise Exception("Invalid sort key, must be one of %s" % self._SORT_KEYS.__repr__())
-        params = { 'page': page,
-                   'sort': sort_key }
+            raise Exception("Invalid sort key, must be one of %s"
+                            % self._SORT_KEYS.__repr__())
+        params = {'page': page,
+                  'sort': sort_key}
         if genre:
             params["category_id"] = genre
         elif country:
@@ -254,7 +275,7 @@ class Mubi(object):
         list_page = self._session.get(list_url)
         num_pages = ceil(int(BS(list_page.content)
                          .find("strong", {"id": "result_count"}).text
-                         .split()[0])/20.0)
+                         .split()[0]) / 20.0)
         return (num_pages, self._parse_watchable_titles(list_page.content))
 
     def get_all_programs(self):
@@ -267,7 +288,8 @@ class Mubi(object):
                 for x in programs]
 
     def get_program_films(self, cinema):
-        program_page = self._session.get("/".join([self._mubi_urls["single_program"], cinema]))
+        program_page = self._session.get("/"
+                .join([self._mubi_urls["single_program"], cinema]))
         items = (BS(program_page.content)
                  .findAll("div", {"class": self._regexps["item"]}))
         films = []
@@ -289,11 +311,7 @@ class Mubi(object):
     def get_watchlist(self, userid=None):
         if not userid:
             userid = self._userid
-        items = [self._get_shortdetails(x)
-                for x in json.loads(
-                    self._session.get(self._mubi_urls["watchlist"] % userid).content)]
-        return [Film(title="%s (%s %s)" % (x['title'], x['primary_country'],
-                                           x['year']),
-                     mubi_id=unicode(x['id']),
-                     filmstill=self._get_filmstill(self._resolve_id(x['id'])))
-                 for x in items]
+        return [self._get_shortdetails(x)
+                for x in json.loads(self._session
+                                    .get(self._mubi_urls["watchlist"] % userid)
+                                    .content)]
